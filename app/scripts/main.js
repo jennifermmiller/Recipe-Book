4607050cb757d19bfac13d72a0c1e15d74b758f2
Jennifer Miller <jmiller6128@gmail.com>
2014-04-25 15:26:10 -0400

    minor changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,24 @@ $(document).ready(function(){
 	router = new MainRouter();
 
 	Backbone.history.start();
+
+	$(".img-social").hover(function(){
+	    $(this).attr("src", function(index, attr){
+	        return attr.replace(".png", "-hover.png");
+	    });
+	}, function(){
+	    $(this).attr("src", function(index, attr){
+	        return attr.replace("-hover.png", ".png");
+	    });
+	});
+
+	$(".img-social-diff").hover(function(){
+	    $(this).attr("src", function(index, attr){
+	        return attr.replace("1.png", "-hover.png");
+	    });
+	}, function(){
+	    $(this).attr("src", function(index, attr){
+	        return attr.replace("-hover.png", "1.png");
+	    });
+	});
 });
